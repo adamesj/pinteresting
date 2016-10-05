ca2771e8aff6791b8348100616b610dd4797d792
Jimmy Adames <jimmy.adames@gmail.com>
2016-10-05 13:33:39 -0400

    added user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_many :pins
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :password, :presence => true,
