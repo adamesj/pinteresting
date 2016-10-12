afe5403460eb2eb26c6cca2eeda9889afd742092
Jimmy Adames <jimmy.adames@gmail.com>
2016-10-12 10:35:53 -0400

    added user validations and replaced user email with name on index and show pages of pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,5 @@ class Pin < ApplicationRecord
                     styles: { medium: "300x300>", thumb: "100x100>" },
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :image, :description, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :pins
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :password, :presence => true,
-                     :on => :create
+  validates :name, :password, :presence => true,
+            :on => create,
+            :format => {:with => /\A.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 end
